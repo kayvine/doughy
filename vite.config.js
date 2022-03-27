7cece3a76f6695b878fa9c7e7fc38667c63a9a82
kayvine <kevin.delme@gmail.com>
2022-03-27 19:06:52 +0200

    undo: chore: use `vite-plugin-pwa`
    This reverts commit 2d41507901dfe8873428ab3e4bac25713eed7225.

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,10 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    createVuePlugin(),
-    VitePWA({
-      includeAssets: ['favicon.ico', 'robots.txt', 'icons/apple-touch-icon.png'],
-      manifest: {
-        name: "Doughy. Love for pastry",
-        short_name: "Doughy",
-        description: "Order for take-out. Or look at our tasty menu.",
-        theme_color: "#ffffff",
-        display: "standalone",
-        display_override: ["window-controls-overlay"],
-        icons: [
-          {
-            "src": "icons/hot-coffee-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/hot-coffee-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
-          }
-        ],
-      }
-    })
-  ],
+  plugins: [createVuePlugin()],
   resolve: {
     alias: [
       {
